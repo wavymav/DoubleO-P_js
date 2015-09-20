0edccf271b7e30c1699356ac8af284cf069c98f5
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-20 19:11:34 -0400

    added the dice roll output functionality

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+// Declaring a dice {} literal
 var dice = {
-	roll: function(sides) {
-				randomNumber =Math.floor(Math.random() * sides) + 1;
-				console.log(randomNumber);
+	// sides property with the value of 6
+	sides: 6,
+	// roll property has an anonymous func() the returns a random number between 1 and 6
+	// roll is a property method
+	roll: function() {
+				randomNumber = Math.floor(Math.random() * this.sides) + 1;
+				return randomNumber;
 	}
+};
+
+// The outputNumber() expression takes one param will be the value of dice.roll()
+// Grabs the #output element and sets the innerHTML val to dice.roll()
+var outputNumber = function(num) {
+	var output = document.getElementById("output");
+	output.innerHTML = num;
+};
+
+// Grabs the #button element
+var button = document.getElementById("button");
+
+// .onclick() fires when #button is clcicked
+// changing the current output val to the current result
+button.onclick = function() {
+	var result = dice.roll();
+	outputNumber(result);
 };
