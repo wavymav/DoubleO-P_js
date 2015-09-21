0b7b9769b76d46ea69514f098912425a85c2f041
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-21 14:24:46 -0400

    added calculator.js and jquery

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ document.addEventListener('DOMContentLoaded', function(){
 
 // Basic calculator object literal
 var calculator = {
-	sum: 0,
+	total: 0,
   add: function(value) {
-    this.sum += value;
+    this.total += value;
   },
   subtract: function(value) {
-    this.sum -= value;
+    this.total -= value;
   },
   multiply: function(value) {
-    this.sum *= value;
+    this.total *= value;
   },
   divide: function(value) {
-    this.sum /= value;
+    this.total /= value;
   },
   clear: function() {
-    this.sum = 0;
+    this.total = 0;
   },
   equals: function() {
-    return this.sum;
+    return this.total;
   }
 };
 
