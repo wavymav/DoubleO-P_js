c2d4a9bdd92d5b2202474cdd6860f0524d33eb96
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-22 14:53:36 -0400

    added functionality to Song & Playlist prototypes

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
 	 this.title = title;
 	 this.artist = artist;
 	 this.duration = duration;
-	 this.isPlaying = isPlaying;
+	 this.isPlaying = false;
 	}
 
 	// Song Methods
-	Song.prototype.play = function() {
 
+	// Changes the boolean value of the Song current object to true
+	Song.prototype.play = function() {
+		this.isPlaying = true;
 	};
 
+	// Changes the boolean value of the Song current object to false
 	Song.prototype.stop = function() {
-
+		this.isPlaying = false;
 	};
 
 	Song.prototype.toHTML = function() {
@@ -23,26 +26,44 @@ document.addEventListener('DOMContentLoaded', function() {
 	//  ===========================================================
 
 	// Playlist constructor properties
-	function Playlist(songs, nowPlaying ) {
-		this.songs = songs;
-		this.nowPlaying = nowPlaying;
+	function Playlist() {
+		this.songs = [];
+		this.nowPlaying = 0;
 	}
 
 	// Playlist Methods
-	Playlist.prototype.add = function() {
 
+	// Adds a Song object to the end of the Playlist songs []
+	Playlist.prototype.add = function(song) {
+		this.songs.push(song);
 	};
 
+	// Access the Song object in the Playlist songs[] using the nowPlaying index number
+	// Then calls the play method on the stored Song Object in currentSong var
 	Playlist.prototype.play = function() {
-
+		var currentSong = this.songs[this.nowPlaying];
+		currentSong.play();
 	};
 
-	Playlist.prototype.next = function() {
-
-	};
-
+	// Access the Song object in the Playlist songs[] using the nowPlaying index number
+	// Then calls the stop method on the stored Song Object in currentSong var
 	Playlist.prototype.stop = function() {
+		var currentSong = this.songs[this.nowPlaying];
+		currentSong.stop();
+	};
 
+	// The next method will call the stop method on the stored Song object
+	// Then increment by 1 up the []
+	// IF the nowPlaying [] is equal to the number of songs
+	// THEN nowPlaying will be reset to 0
+	// After the play method will be invoked
+	Playlist.prototype.next = function() {
+		this.stop();
+		this.currentSong++;
+		if (this.nowPlaying === this.songs.length) {
+			this.nowPlaying = 0;
+		}
+		this.play();
 	};
 
 	Playlist.prototype.renderElement = function() {
